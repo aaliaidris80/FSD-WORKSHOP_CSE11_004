7a00f3761868fa30e7b3d75bcab067ce2a14ae74
aaliaidris80 <aaliaidris80@gmail.com>
2026-08-27 14:23:30 +0530

    lab2 file creation

--- Backend/LAB2/FileDemo.js
+++ Backend/LAB2/FileDemo.js
@@ -19,7 +19,25 @@ async function readFile() {
         console.log("Error reading file:", error);
     }
 }
-
+async function appendFile(content){
+    try{
+        await fs.appendFile(filePath,content,"utf8");
+        console.log("Content appended successfully");
+    } catch (error) {
+        console.log("Error appending to file:", error);
+    }
+}
+async function deleteFile(){
+    try{
+        await fs.unlink(filePath);
+        console.log("File deleted successfully");
+    } catch (error) {
+        console.log("Error deleting file:", error);
+}
+}
 // Function calls
 await createFile("Hello, this is a sample content for the file.");
 await readFile();
+await appendFile("\nThis is an appended line.");
+await readFile();
+// await deleteFile();
